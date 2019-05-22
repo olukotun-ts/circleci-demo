0388b9e2aff79790630be90ac98a431f00aadf4b
Sam Olukotun <sam.olukotun@circleci.com>
2019-05-21 23:36:42 -0600

    Show metadata

--- CircleData.js
+++ CircleData.js
@@ -1,21 +1,38 @@
-const chance = require('chance').Chance();
+const axios = require('axios');
 
 module.exports = {
     data() {
         return {
+            metadata: null,
             name: 'Click here'
         }
     },
 
+    mounted() {
+        this.getMetadata();
+    },
+
     methods: {
-        generateName() {
-            this.name = chance.name({ suffix: true });
+        getMetadata() {
+            axios.get('/metadata')
+            .then(({ data }) => {
+                this.metadata = data.cci_metadata;
+            })
+            .catch(error => {
+                const message = 'Error getting metadata';
+                console.error(message, '\n', error);
+                this.metadata = message;
+            })
         }
     },
 
     template: `
-        <label> Your second id:
-            <button id="btn-main" v-on:click="generateName">{{ name }}</button>
-        </label>
+        <div>
+            <ul> 
+                <li v-for="(value, key) in metadata">
+                    {{ key }}: {{ value }}
+                </li>
+            </ul>
+        </div>
     `
 };
