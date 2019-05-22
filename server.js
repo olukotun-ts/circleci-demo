ffcfaf2ed800bf16f1d8b204cad78af724958373
Sam Olukotun <sam.olukotun@circleci.com>
2019-05-21 23:34:35 -0600

    Add metadata endpoint

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
+require('dotenv').config();
+const axios = require('axios');
 const express = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
 app.use('/static', express.static('public'));
+
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
 });
+
+app.get('/metadata', (req, res) => {
+    axios.get('https://circleci.com/api/v1.1/project/github/olukotun-ts/name-button', {
+        params: {
+            'circle-token': process.env.CIRCLE_API_TOKEN,
+            limit: 1
+        }
+    })
+    .then(({ data }) => {
+        const {
+            committer_email,
+            committer_name,
+            reponame,
+            subject
+        } = data[0];
+
+        res.send({ cci_metadata: {
+            committer_email,
+            committer_name,
+            reponame,
+            subject
+        }});
+    })
+    .catch(error => {
+        console.log('Error: GET /metadata');
+        console.log(error);
+
+        res.status(500).send(error);
+    })
+})
+
 app.listen(port, () => console.log(`Listening on port ${port}.`));
