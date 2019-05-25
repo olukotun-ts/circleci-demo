b00490443dac19f73652bf8a99c39a07d4428ec2
Sam Olukotun <sam.olukotun@circleci.com>
2019-05-24 19:46:27 -0600

    Init template

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use('/static', express.static('public'));
+app.use('/', express.static('.'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: __dirname });
